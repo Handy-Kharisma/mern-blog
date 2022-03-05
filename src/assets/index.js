6c8f3b4b1264e68b5f0083669ed227cb6a723133
Handy-Kharisma <handykharisma75@gmail.com>
2022-03-05 18:31:38 +0700

    third commit

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import RegisterBg from './image/georgia-de-lotz-juQWcOiuuPE-unsplash.jpg';
+import LoginBg from './image/toa-heftiba-ip9R11FMbV8-unsplash.jpg';
+
+export { RegisterBg, LoginBg };
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import './register.scss';
+import { RegisterBg } from '../../assets';
+import { Button, Gap, Input, Link } from '../../components';
 
 const Register = () => {
   return (
-    <div>
-        <p>Register Page</p>
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} className="bg-image" alt='ImageBg'/>
+      </div>
+      <div className="right">
+        <p className="title">Register</p>
+        <Input label="Full Name" placeholder="Full Name" />
+        <Gap height={18} />
+        <Input label="Email" placeholder="Email"/>
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={30} />
+        <Button title="Register" />
+        <Gap height={100} />
+        <Link title="Kembali Ke Login" />
+      </div>
     </div>
   )
 }
