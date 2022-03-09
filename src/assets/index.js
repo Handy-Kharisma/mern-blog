a8e218e689338e586a2f3a0a7ced9a20b77eb994
Handy-Kharisma <handykharisma75@gmail.com>
2022-03-09 11:18:11 +0700

    sixth commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,6 +2,7 @@ import RegisterBg from './image/georgia-de-lotz-juQWcOiuuPE-unsplash.jpg';
 import LoginBg from './image/toa-heftiba-ip9R11FMbV8-unsplash.jpg';
 import LogoHK from './image/Handy_Large_NoBG.png';
 import BlogImage from './image/thought-catalog-505eectW54k-unsplash.jpg';
+import CreateImage from './image/john-mark-smith-P6uqpNyXcI4-unsplash.jpg';
 // icon
 import ICFacebook from './icon/Facebook.svg';
 import ICTwitter from './icon/Twitter.svg';
@@ -10,4 +11,4 @@ import ICTelegram from './icon/Telegram.svg';
 import ICDiscord from './icon/Discord.svg';
 import ICGitHub from './icon/GitHub.svg';
 
-export { RegisterBg, LoginBg, LogoHK, BlogImage, ICFacebook, ICTwitter, ICInstagram, ICTelegram, ICDiscord, ICGitHub };
+export { RegisterBg, LoginBg, LogoHK, BlogImage, CreateImage, ICFacebook, ICTwitter, ICInstagram, ICTelegram, ICDiscord, ICGitHub };
